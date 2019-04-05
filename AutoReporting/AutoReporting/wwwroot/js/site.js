963e323b7d49f993ad75ab298888d65fcf250855
Andrew Skeklios <Skeksalot@users.noreply.github.com>
2019-04-05 14:46:16 +1030

    Updates

--- AutoReporting/AutoReporting/wwwroot/js/site.js
+++ AutoReporting/AutoReporting/wwwroot/js/site.js
@@ -2,3 +2,35 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function DownloadAll() {
+
+	// Load full trainer list
+
+	// Cycle through list and download reports
+
+	// Inform the user
+	alert("All trainer reports downloaded.");
+	return;
+}
+
+function DownloadSelected() {
+
+	// Grab trainer lsit from input
+	var input = document.getElementById("trainer-list");
+
+	// Input Validation with HTML5 pattern matching
+	if ( !input.checkValidity() ) {
+		// Invalid input, report back to the user
+		alert(input.validationMessage);
+	} else {
+		// Names entered and ok
+		// Split out individual trainers
+
+		// Download reports
+
+		// Inform the user
+		alert("Selected trainer reports downloaded:\n" + input.value);
+	}
+	return;
+}
