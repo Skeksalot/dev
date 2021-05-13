8a524ee600a72b862c327a657981c2c0b18ac4d2
Andrew Skeklios <sir.skeksalot@gmail.com>
2021-05-13 09:40:03 +0930

    Looks like a commit was missed. Updating with what's on the Moodle.

--- LMS Reports/Test - Logs Affected User.sql
+++ LMS Reports/Test - Logs Affected User.sql
@@ -3,15 +3,20 @@ https://lms.upskilled.edu.au/blocks/configurable_reports/viewreport.php?id=299
 */
 
 SELECT DISTINCT CONCAT(u.firstname, ' ', u.lastname) User, CONCAT(realuser.firstname, ' ', realuser.lastname) Real_User, CONCAT(related.firstname, ' ', related.lastname) Related_User,
-	c.shortname Course, log.action, log.target, FROM_UNIXTIME(log.timecreated) Time_Created
+	c.shortname Course, Logs.action, Logs.target, FROM_UNIXTIME(Logs.timecreated) Time_Created
 
-FROM prefix_logstore_standard_log log
-JOIN prefix_user u ON log.userid = u.id
-JOIN prefix_user realuser ON log.realuserid = realuser.id
-JOIN prefix_user related ON log.relateduserid = related.id
-JOIN prefix_course c ON log.courseid = c.id
+FROM (
+	SELECT log.userid, log.realuserid, log.relateduserid, log.courseid, log.action, log.target, log.timecreated
+	FROM prefix_logstore_standard_log log
+-- User ID of affected user
+	WHERE ( log.userid = 15790 OR log.relateduserid = 15790 )
+	%%FILTER_STARTTIME:Logs.timecreated:>%%
+	%%FILTER_ENDTIME:Logs.timecreated:<%%
+) Logs
+JOIN prefix_user u ON Logs.userid = u.id
+JOIN prefix_user realuser ON Logs.realuserid = realuser.id
+JOIN prefix_user related ON Logs.relateduserid = related.id
+JOIN prefix_course c ON Logs.courseid = c.id
 
-WHERE log.relateduserid IS NOT NULL
+WHERE Logs.relateduserid IS NOT NULL
 %%FILTER_SEARCHTEXT:CONCAT(related.firstname, ' ', related.lastname):~%%
-%%FILTER_STARTTIME:log.timecreated:>%%
-%%FILTER_ENDTIME:log.timecreated:<%%
